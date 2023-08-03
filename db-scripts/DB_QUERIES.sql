b10336279610dfbc9baa0f5b81f247f408f8d1ba
karthikathota <saikarthik.tech@gmail.com>
2023-08-03 19:16:05 +0530

    DB scripts are updated

--- db-scripts/DB_QUERIES.sql
+++ db-scripts/DB_QUERIES.sql
@@ -1,12 +1,5 @@
-select  count(*) from users where UserName ='123';
-select * from users;
-ALTER TABLE Users
-DROP COLUMN UserID;
-Insert into users (PhoneNo, FirstName, LastName, UserName, Email, UserPassword) values
-				("9923123232", "Sai", "Karthik", "123", "saikarthik.athota@gmail.com","123");
+CREATE DATABASE dairy;               
                 
-                
-SHOW CREATE TABLE Users;
 CREATE TABLE `users` (
    `UserID` INT PRIMARY KEY auto_increment,
    `PhoneNo` VARCHAR(50) NOT NULL,
@@ -16,7 +9,6 @@ CREATE TABLE `users` (
    `Email` VARCHAR(50) NOT NULL,
    `UserPassword` VARCHAR(50) NOT NULL
 );
-SHOW CREATE TABLE dairy;
 CREATE TABLE `dairy` (
    `Id` int NOT NULL AUTO_INCREMENT,
    `Entry` text NOT NULL,
@@ -27,11 +19,3 @@ CREATE TABLE `dairy` (
    KEY `UserID` (`UserID`),
    CONSTRAINT `dairy_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`)
  );
- 
- Insert into dairy (Entry, DairyDate, UserID) values ("This is todays Dairy", "2023-07-30" , 12);
-                
-select * from dairy where UserID=12 && DairyDate="2023-07-27" order by ID desc;
-select * from dairy where UserID=12 order by ID desc;
-
-select * from dairy;
-
